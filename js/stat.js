0e321b230967953e23b51cdb0b1a59e937ea43f7
tatalex <burmina7@gmail.com>
2019-06-08 16:32:57 +0300

    добавляет переменные и функцию для отрисовки статистики

--- js/stat.js
+++ js/stat.js
@@ -22,6 +22,13 @@ var FONT_POSITION_SUBTITLE_Y = 70;
 var playerName = 'Вы';
 var playerColor = 'rgba(255, 0, 0, 1)';
 
+//Statistic bar settings
+var FONT_GAP = 50;
+var STATISTIC_MAX_HEIGHT = 120;
+var STATISTIC_WIDTH = 40;
+var statisticHeight = CLOUD_HEIGHT - GAP - STATISTIC_MAX_HEIGHT - GAP;
+
+
 var renderCloud = function(ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
@@ -43,5 +50,32 @@ var statisticColor = function (ctx, players) {
     ctx.fillStyle = playerColor;
   } else {
     ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
+  }
+};
+
+window.renderStatistics = function(ctx, players, times) {
+  var maxTime = getMaxElement(times);
+  //Clouds
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, SHADOW_COLOR);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, WHITE_COLOR);
+
+  //Statistic text
+  ctx.fillStyle = BLACK_COLOR;
+  ctx.font = FONT_CHARACTERISTIC;
+  ctx.fillText("Ура вы победили!", FONT_POSITION_X, FONT_POSITION_TITLE_Y);
+  ctx.fillText("Список результатов:", FONT_POSITION_X, FONT_POSITION_SUBTITLE_Y);
+
+  //Statistic bar
+  for (var i = 0; i < players.length; i++) {
+    statisticColor(ctx, players[i]);
+    times[i] = Math.round(times[i]);
+    var statisticPositionX = CLOUD_X + GAP + STATISTIC_WIDTH + (STATISTIC_WIDTH + FONT_GAP) * i;
+    //Statistc rectangles
+    ctx.fillRect(statisticPositionX, CLOUD_Y + GAP + CLOUD_HEIGHT - FONT_GAP - (statisticHeight * times[i]) / maxTime, STATISTIC_WIDTH, (statisticHeight * times[i]) / maxTime);
+
+    //Statistc names and times
+    ctx.fillStyle = BLACK_COLOR;
+    ctx.fillText(players[i], statisticPositionX, CLOUD_HEIGHT + CLOUD_Y - 2 * GAP);
+    ctx.fillText(times[i], statisticPositionX, CLOUD_Y + GAP + CLOUD_HEIGHT - FONT_GAP - (statisticHeight * times[i]) / maxTime - GAP);
   }
 };
