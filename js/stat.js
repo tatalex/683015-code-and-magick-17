8a20288c8f48d8c2fab9b13327cdb53ba31ab10c
tatalex <burmina7@gmail.com>
2019-06-08 15:33:39 +0300

    добавляет переменные и константы для отрисовки облака и текста

---
+++ js/stat.js
@@ -0,0 +1,19 @@
+'use strict';
+
+//Cloud settings
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+
+//Colors
+var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
+var WHITE_COLOR = '#fff';
+var BLACK_COLOR = '#000';
+
+//Font settings
+var FONT_CHARACTERISTIC = "16pt PT Mono";
+var FONT_POSITION_X = 120;
+var FONT_POSITION_TITLE_Y = 40;
+var FONT_POSITION_SUBTITLE_Y = 70;
