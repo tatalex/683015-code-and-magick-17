0fc51f6d31ed8c96c71e3396a7fd88af2a58d207
tatalex <burmina7@gmail.com>
2019-06-15 22:10:26 +0300

    fixes bugs after mentor review

--- js/setup.js
+++ js/setup.js
@@ -1,19 +1,10 @@
 'use strict';
 
-// removes classes .hidden
-document.querySelector('.setup').classList.remove('hidden');
-document.querySelector('.setup-similar').classList.remove('hidden');
-
-
-var CHARACTER_AMOUNT = 4;
+var CHARACTERS_AMOUNT = 4;
 var NAMES = ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
-
-var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-var fragment = document.createDocumentFragment();
 
 // creates characters
 var getRandomInRange = function (min, max) {
@@ -37,10 +28,9 @@ var createCharacters = function (count) {
   return wizards;
 };
 
-var characterOfWizards = createCharacters(CHARACTER_AMOUNT);
-
 // renders character
-var renderCharacter = function (wizard) {
+var renderWizard = function (wizard) {
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
   var characterWizard = similarWizardTemplate.cloneNode(true);
   characterWizard.querySelector('.setup-similar-label').textContent = wizard.name;
   characterWizard.querySelector('.wizard-coat').style.fill = wizard.coatColor;
@@ -50,12 +40,21 @@ var renderCharacter = function (wizard) {
 };
 
 // adds characters to the table
-var addWizards = function () {
+var renderWizards = function () {
+  var similarListElement = document.querySelector('.setup-similar-list');
+  var fragment = document.createDocumentFragment();
+  var characterOfWizards = createCharacters(CHARACTERS_AMOUNT);
+
   for (var i = 0; i < characterOfWizards.length; i++) {
-    fragment.appendChild(renderCharacter(characterOfWizards[i]));
+    fragment.appendChild(renderWizard(characterOfWizards[i]));
   }
 
   similarListElement.appendChild(fragment);
 };
 
-addWizards();
+renderWizards();
+
+
+// removes classes .hidden
+document.querySelector('.setup').classList.remove('hidden');
+document.querySelector('.setup-similar').classList.remove('hidden');
