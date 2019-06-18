a6eea59c0bfb12edc6894b1637fededa59d894c6
tatalex <burmina7@gmail.com>
2019-06-18 13:52:00 +0300

    adds tabindex, limits to the field username and configures opening and closing of the settings window

--- js/setup.js
+++ js/setup.js
@@ -5,6 +5,18 @@ var NAMES = ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кри
 var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+
+
+var form = document.querySelector('.setup-wizard-form');
+var userName = form.querySelector('.setup-user-name');
 
 var similarListElement = document.querySelector('.setup-similar-list');
 
@@ -54,7 +66,45 @@ var renderWizards = function () {
 
 similarListElement.appendChild(renderWizards());
 
-
 // removes classes .hidden
 document.querySelector('.setup').classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');
+
+
+// configures opening and closing of the settings window
+var onPopupEscPress = function(evt) {
+  if (evt.keyCode === ESC_KEYCODE && userName !== document.activeElement) {
+    closePopup();
+  }
+  return evt;
+};
+
+var openPopup = function() {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function() {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function() {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function() {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
